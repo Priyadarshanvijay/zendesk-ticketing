89ae3fedf5afa417b54b79bb0b02e7be1ee41f80
Priyadarshan Vijay <priyadarshan@nyu.edu>
2021-11-26 16:26:24 -0500

    feat: added next and prev batch pagination

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import React, { createRef } from 'react';
+import React from 'react';
 import TicketService from './services/ticketService';
 import Ticket from './components/ticket';
-import { Container, GridColumn, Grid, Segment, Sticky } from 'semantic-ui-react';
+import { Container, GridColumn, Grid, Segment } from 'semantic-ui-react';
 import TopMenubar from './components/topMenubar';
 import ArrayUtils from './utils/arrayutils';
 import PaginationBar from './components/paginationBar';
@@ -15,28 +15,88 @@ function App() {
   const [tickets, setTickets] = useState([]);
   const [users, setUsers] = useState({});
   const [loadedTickets, setLoadedTickets] = useState(false);
-  const [isSelected, setisSelected] = useState(null);
-  useEffect(() => {
-    ticketService.getTickets()
-    .then(appendTickets)
+  const [isSelected, setIsSelected] = useState(NaN);
+  const [requester, setRequester] = useState({});
+  const [assignee, setAssignee] = useState({});
+  const [selectedTicket, setSelectedTicket] = useState({});
+  const [curPage, setCurPage] = useState(1);
+  const [hasMore, setHasMore] = useState(false);
+  const [hasLess, setHasLess] = useState(false);
+  const [prevCursor, setPrevCursor] = useState('');
+  const [nextCursor, setNextCursor] = useState('');
+  const [paginationOffset, setPaginationOffset] = useState(0);
+
+  const getCursorToFetch = (isBefore = false) => isBefore ? { before: prevCursor } : { after: nextCursor };
+
+  const changePaginationOffset = (isBefore = false, isFirst = false) => (data) => {
+    if (!isFirst) {
+      if (isBefore) {
+        setPaginationOffset((prev) => (prev - 1));
+      } else {
+        setPaginationOffset((prev) => (prev + 1));
+      }
+    }
+    return data;
+  };
+
+  const updateCursors = (data) => {
+    setHasMore(data?.meta?.has_more || false);
+    setPrevCursor(data?.meta?.before_cursor);
+    setNextCursor(data?.meta?.after_cursor);
+    return data;
+  }
+
+  const loadTickets = (isBefore = false, isFirst = false) => () => {
+    setLoadedTickets(false);
+    return ticketService.getTickets(getCursorToFetch(isBefore))
+    .then(updateCursors)
+    .then(insertTickets)
     .then(appendUsers)
+    .then(changePaginationOffset(isBefore, isFirst))
     .then(setLoadedTickets);
+  }
+  
+  useEffect(() => {
+    loadTickets(false, true)();
   }, []);
 
-  const contextRef = createRef();
+  useEffect(() => {
+    setHasLess(paginationOffset > 0);
+    resetTicketSelect();
+    setCurPage(1);
+    console.log("curPage", curPage);
+    console.log("isSelected", isSelected)
+  }, [paginationOffset])
 
-  const appendTickets = (data) => {
-    setTickets((prev = []) => {
+  const insertTickets = (data) => {
+    setTickets(() => {
       const chunksOfTwentyFive = ArrayUtils.arrayChunks(25)(data.tickets);
-      return [...prev, ...chunksOfTwentyFive];
+      return [...chunksOfTwentyFive];
     });
     return data;
   };
 
   const appendUsers = (data) => {
-    setUsers((prev = {}) => ({ ...prev, ...(data?.users || {}) }));
+    setUsers(() => ({ ...(data?.users || {}) }));
     return true;
+  };
+
+  const resetTicketSelect = () => {
+    setIsSelected(NaN);
+    setSelectedTicket((_) => ({}));
+    setRequester((_) => ({}));
+    setAssignee((_) => ({}));
   }
+
+  const selectTicket = (ticketId) => () => {
+    setIsSelected(ticketId);
+    const newTicket = tickets?.[(curPage - 1) % 4]?.[ticketId] || {};
+    setSelectedTicket(newTicket);
+    const newRequester = users[newTicket.requester_id];
+    setRequester(newRequester);
+    const newAssignee = users[newTicket.assignee_id];
+    setAssignee(newAssignee);
+  };
   
   if (!loadedTickets) {
     return (
@@ -52,20 +112,22 @@ function App() {
   return (
     <>
     <TopMenubar/>
-    <Container fluid style={{ height: "100%", paddingTop: '7em', paddingBottom: '4em' }}>
+    <Container fluid style={{ height: '100%', paddingTop: '7em', paddingBottom: '4em' }}>
     <Grid columns='2' style={{ height:'100%' }} stackable>
-      <GridColumn style={{ height: "100%", overflow: "auto", paddingTop: '0em' }}>
+      <GridColumn style={{ height: '100%', overflow: 'auto', paddingTop: '0em' }}>
         <Segment basic>
-          {(tickets?.[1] || []).map((ticket) => (<Ticket 
-          selected={isSelected === ticket.id} onChange = {() => setisSelected(ticket.id)} key={ticket.id} ticketDetails={ticket}></Ticket>))}
+          {(tickets?.[(curPage - 1) % 4] || []).map((ticket, index) => (<Ticket 
+          selected={isSelected === index} onChange = {selectTicket(index)} key={ticket.id} ticketDetails={ticket}></Ticket>))}
         </Segment>
       </GridColumn>
-      <GridColumn style={{ height: "100%" }}>
-        <TicketDetailedView />
+      <GridColumn style={{ height: '100%' }}>
+      {!isNaN(isSelected) &&
+        <TicketDetailedView ticket={selectedTicket} requester={requester} assignee={assignee} />
+      }
       </GridColumn>
     </Grid>
     </Container>
-    <PaginationBar tickets={(tickets|| [])}/>
+    <PaginationBar loadTickets={loadTickets} hasNextItem={hasMore} hasPrevItem={hasLess} paginationOffset={paginationOffset} tickets={(tickets|| [])} activePage={curPage} setPage={setCurPage}/>
     </>
   );
 }
--- client/src/components/ticketDetailed.js
+++ client/src/components/ticketDetailed.js
@@ -1,13 +1,57 @@
 import React, { createRef } from 'react'
-import { Sticky, Container, Header, Image, Modal, ModalActions, Segment, Menu } from 'semantic-ui-react'
+import { Grid, Header, Segment, Feed, Divider, Label } from 'semantic-ui-react'
 
-function TicketDetailedView() {
+function TicketDetailedView({ ticket, requester, assignee }) {
+
+  if (Object.keys(ticket || {}).length === 0) {
+    return <></>
+  }
 
   return (
     <Segment basic fluid>
       <Header>
-        This is a post
+        {ticket.subject}
       </Header>
+      <Divider />
+      <Header style={{ fontSize: '1em' }}>
+        Description: </Header>
+      {ticket.description}
+      <Grid style={{ marginTop: '3em' }} columns='2'>
+        <Grid.Column>
+          <Feed>
+        <Header style={{ fontSize: '1em' }}>
+          Assignee </Header>
+          <Divider />
+        <Feed.Event>
+          {assignee.profilePhoto && <Feed.Label image={assignee.profilePhoto} />}
+          <Feed.Content>
+            <Feed.Summary>
+              {assignee.name}
+            </Feed.Summary>
+          </Feed.Content>
+        </Feed.Event>
+        </Feed>
+        </Grid.Column>
+        {/* <Grid.Column /> */}
+      <Grid.Column>
+      <Feed>
+        <Header style={{ fontSize: '1em' }}>
+          Requester </Header>
+          <Divider />
+        <Feed.Event>
+          {requester.profilePhoto && <Feed.Label image={requester.profilePhoto} />}
+          <Feed.Content>
+            <Feed.Summary>
+              {requester.name}
+            </Feed.Summary>
+          </Feed.Content>
+        </Feed.Event>
+      </Feed>
+      </Grid.Column>
+      </Grid>
+      <Segment basic>
+      {ticket.tags?.map((tag) => <Label size='mini' color='teal' tag>{tag}</Label>)}
+      </Segment>
     </Segment>
   )
 }
